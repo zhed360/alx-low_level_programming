0e74ffda1ed6e8203e10044f2f1fe7ed08192752
zhed360 <hasram2020@yahoo.com>
2023-08-01 15:27:01 -0700

    updated

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,11 +1,11 @@
-#include "list.h"
+#include "lists.h"
 
 /**
  * listint_len - return the number of elements in a inked list
  * @h: linked list of type listint_t to traverse
  *
  * Return: number of nodes
- * /
+ */
 size_t listint_len(const listint_t *h)
 {
 	size_t num = 0;
